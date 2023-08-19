f14c4206691770b40f2170ff446bcbf0faa1af46
lukecoghlan <142559969+lukecoghlan@users.noreply.github.com>
2023-08-19 11:36:54 -0500

    now uses \n and \t

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -4,9 +4,10 @@
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
-   /* Here is how
-   to have
-   multi-line
-   comments. */
+   /* Here is the
+   way that you add
+   multi-line comments */
 
-   console.log("Comments make your code more readable by others.");
+   // console.log("Hello, Comments!")
+
+  console.log("Hello, Console!")
